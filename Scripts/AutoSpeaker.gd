d155574dc6ff97aa566e1624f4ef4cf163299a22
Joel Robert Justiawan <joelwindows7@gmail.com>
2021-01-07 16:20:05 +0700

    produal
    why is it lagging even I disabled sound every button?!

--- Scripts/AutoSpeaker.gd
+++ Scripts/AutoSpeaker.gd
@@ -1,7 +1,7 @@
 extends AudioStreamPlayer
 
 var FLMmusic = FLMusicLib.new()
-var ButtonSound = AudioStreamPlayer.new()
+#var ButtonSound = AudioStreamPlayer.new()
 var ButtonSoundFX : String = "res://Audio/EfekSuara/448081__breviceps__tic-toc-click.wav"
 #var ArlezMIDI = MidiPlayer.new()
 # Declare member variables here. Examples:
@@ -25,8 +25,8 @@ func _ready():
 	# https://gamedev.stackexchange.com/questions/184354/add-a-sound-to-all-the-buttons-in-a-project/184363#184363
 	# https://github.com/godotengine/godot-proposals/issues/1472
 	# Godot pls make it a theme feature! https://github.com/godotengine/godot/issues/3608
-	add_child(ButtonSound)
-	ButtonSound.stream = load(ButtonSoundFX)
+	#add_child(ButtonSound)
+	#ButtonSound.stream = load(ButtonSoundFX)
 #	connect_buttons(get_tree().root)
 #	get_tree().connect("node_added", self, "_on_SceneTree_node_added")
 	# Breviceps click https://freesound.org/people/Breviceps/sounds/448081/
@@ -34,21 +34,25 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_SceneTree_node_added(node):
-	if node is Button:
-		connect_to_button(node)
+#	if node is Button:
+#		connect_to_button(node)
+	pass
 
 func _on_Button_pressed():
-	ButtonSound.play()
+	#ButtonSound.play()
+	pass
 
 # recursively connect all buttons
 func connect_buttons(root):
-	for child in root.get_children():
-		if child is BaseButton:
-			connect_to_button(child)
-		connect_buttons(child)
+#	for child in root.get_children():
+#		if child is BaseButton:
+#			connect_to_button(child)
+#		connect_buttons(child)
+	pass
 
 func connect_to_button(button):
-	button.connect("pressed", self, "_on_Button_pressed")
+#	button.connect("pressed", self, "_on_Button_pressed")
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
